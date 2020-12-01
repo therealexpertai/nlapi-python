5e755e9ab3e6ee0ffc8d7fbd745901b04a14b6b8
marcobellei-eai <mbellei@MBELLEI>
2020-12-01 11:48:45 +0100

    changed version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="expertai-nlapi",
-    version="2.1.3",
+    version="2.1.4",
     author="Expert System S.p.A.",
     author_email="api.support@expert.ai",
     description="Python client for expert.ai Natural Language API",
