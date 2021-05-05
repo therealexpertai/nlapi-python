4ea48614f5fa6385fc5288bebc02a770a3b3848e
avarone <avarone@expertsystem.com>
2021-05-05 16:37:51 +0200

    Provision for next patch version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="expertai-nlapi",
-    version="2.3.0",
+    version="2.3.1",
     author="Expert System S.p.A.",
     author_email="api.support@expert.ai",
     description="Python client for expert.ai Natural Language API",
